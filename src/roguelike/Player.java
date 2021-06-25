c73e966bfc6ed9f5337da317f8f6b516d7e3e3fc
Anthony Howell <anthonyamh@gmail.com>
2021-06-24 23:35:43 -0500

    Added movement code

--- src/roguelike/Player.java
+++ src/roguelike/Player.java
@@ -9,10 +9,10 @@ package roguelike;
  *
  * @author Anthony
  */
-public class Player {
+public class Player extends GameObject{
     
     int health, xposition, yposition;
-    char character = '@';
+    char symbol = '@';
     Board gameboard;
     
     public Player(int health, int xposition, int yposition, Board gameboard){
@@ -20,15 +20,24 @@ public class Player {
         this.xposition = xposition;
         this.yposition = yposition;
         this.gameboard = gameboard;
+        
+        gameboard.SetObjectAtSquare(xposition, yposition, this);
     }
     
     public void MoveUp(){
         //Check if space is empty
-        if ((gameboard.GetObjectAtSquare(xposition, yposition - 1)).isSolid == false){
-            
-            //Move player upwards
-            yposition--;
-            
+        if ((gameboard.GetObjectAtSquare(xposition, yposition - 1)) == null ||
+            (gameboard.GetObjectAtSquare(xposition, yposition - 1)).isSolid == false){
+
+                //Remove current position in board
+                gameboard.SetObjectAtSquare(xposition, yposition, null);
+
+                //Update player's positional variables
+                yposition--;
+
+                //Set player's board position
+                gameboard.SetObjectAtSquare(xposition, yposition, this);
+
         }
         
         //If not, perform contextual behavior based on object in square
@@ -36,13 +45,63 @@ public class Player {
     
     public void MoveDown(){
         //Check if space is empty
-        if ((gameboard.GetObjectAtSquare(xposition, yposition + 1)).isSolid == false){
-            
-            //Move player upwards
-            yposition++;
-            
+       if ((gameboard.GetObjectAtSquare(xposition, yposition + 1)) == null ||
+            (gameboard.GetObjectAtSquare(xposition, yposition + 1)).isSolid == false){
+
+                //Remove current position in board
+                gameboard.SetObjectAtSquare(xposition, yposition, null);
+
+                //Update player's positional variables
+                yposition++;
+
+                //Set player's board position
+                gameboard.SetObjectAtSquare(xposition, yposition, this);
+
         }
         
         //If not, perform contextual behavior based on object in square
+    }
+    
+    public void MoveRight(){
+        //Check if space is empty
+       if ((gameboard.GetObjectAtSquare(xposition + 1, yposition)) == null ||
+            (gameboard.GetObjectAtSquare(xposition + 1, yposition)).isSolid == false){
+
+                //Remove current position in board
+                gameboard.SetObjectAtSquare(xposition, yposition, null);
+
+                //Update player's positional variables
+                xposition++;
+
+                //Set player's board position
+                gameboard.SetObjectAtSquare(xposition, yposition, this);
+
+        }
+        
+        //If not, perform contextual behavior based on object in square
+    }
+    
+    public void MoveLeft(){
+        //Check if space is empty
+       if ((gameboard.GetObjectAtSquare(xposition - 1, yposition)) == null ||
+            (gameboard.GetObjectAtSquare(xposition - 1, yposition)).isSolid == false){
+
+                //Remove current position in board
+                gameboard.SetObjectAtSquare(xposition, yposition, null);
+
+                //Update player's positional variables
+                xposition--;
+
+                //Set player's board position
+                gameboard.SetObjectAtSquare(xposition, yposition, this);
+
+        }
+        
+        //If not, perform contextual behavior based on object in square
+    }
+
+    @Override
+    public char GetSymbol() {
+        return symbol;
     }
 }
