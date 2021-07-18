02446c085cda70d153182d5f13a348eefce268c7
Anthony Howell <anthonyamh@gmail.com>
2021-07-18 00:00:59 -0500

    Moved a* algorithm function to calculatePath()

--- src/roguelike/Node.java
+++ src/roguelike/Node.java
@@ -1,17 +1,14 @@
-/*
- * Represents a board coordinate and contains a trail of parents nodes used to 
- * create a path
- */
 package roguelike;
 
 import java.util.PriorityQueue;
 
 /**
- *
  * @author Anthony
+ * 
+ * Represents a board coordinate and contains a trail of parents nodes used to create a path
  */
 public class Node implements Comparable<Node> {
-    
+
     /* Initialize variables */
     //Positional
     public int x, y;
@@ -41,6 +38,17 @@ public class Node implements Comparable<Node> {
 
     }
     
+    public static boolean containsNode(PriorityQueue q, Node n){
+        
+        for (Object o : q){
+            if (n.equals(o)){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
     @Override
     public boolean equals(Object o){
 
@@ -57,15 +65,75 @@ public class Node implements Comparable<Node> {
         return ((x == n.x) && (y == n.y));
     }
     
-    public static boolean containsNode(PriorityQueue q, Node n){
+    //Calculate path using a* algorithm
+    public static Node calculatePath(int xstart, int ystart, int xgoal, int ygoal, Board gameboard){
         
-        for (Object o : q){
-            if (n.equals(o)){
-                return true;
+        //Create node to represent target
+        Node target = new Node(xgoal, ygoal, 0);
+        
+        //Initialize list for open squares and add starting square
+        PriorityQueue<Node> openList = new PriorityQueue<>();
+        openList.add(new Node(xstart, ystart, 0));
+        
+        //Initialize list for closed squares
+        PriorityQueue closedList = new PriorityQueue<>();
+        
+        //While the open list is not empty
+        while (!openList.isEmpty()){
+            
+            //Get the node that is currently closest to the goal
+            Node n = openList.peek();
+            
+            //Get q's four neighbors
+            Node[] neighbors = new Node[4];
+            neighbors[0] = new Node(n.x + 1, n.y, 0);
+            neighbors[1] = new Node(n.x - 1, n.y, 0);
+            neighbors[2] = new Node(n.x, n.y + 1, 0);
+            neighbors[3] = new Node(n.x, n.y - 1, 0);
+            
+            //Go through each neighor
+            for (Node m : neighbors) {
+
+                //If the node isn't in the closed list or open list
+                if(!Node.containsNode(openList, m) && !Node.containsNode(closedList, m)){
+                    
+                    //If the node is the target, exit
+                    if ((m.x == xgoal) && (m.y == ygoal)){
+                        
+                        m.parent = n;
+
+                        return m;
+                        
+                    }
+                    
+                    //If the node is occupyied by a solid object, add it directly to the closed list
+                    if (gameboard.checkIfSquareHasSolid(m.x, m.y)) {
+
+                        closedList.add(m);
+                        
+                    }
+                    
+                    //Else, calculate its heuristic and add it to the openlist
+                    else {
+
+                        m.parent = n;
+                        m.f = m.calculateHeuristic(target);
+                        openList.add(m);
+                        
+                    }
+                    
+                }
+                
             }
+            
+            //Move current node to closedList
+            openList.remove(n);
+            closedList.add(n);
+            
         }
         
-        return false;
+        return null;
+        
     }
 
 }
