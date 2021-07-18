3aaeb7c50679a75e75f96b18261658ff6d4aa8ac
Anthony Howell <anthonyamh@gmail.com>
2021-07-18 00:03:57 -0500

    Implemented combat logic

--- src/roguelike/objects/entities/goblin/GoblinStateChase.java
+++ src/roguelike/objects/entities/goblin/GoblinStateChase.java
@@ -3,10 +3,9 @@
  */
 package roguelike.objects.entities.goblin;
 
-import java.util.PriorityQueue;
+import roguelike.Board;
 import roguelike.Node;
 import roguelike.objects.entities.ParentEntity;
-import roguelike.objects.entities.player.Player;
 import roguelike.State;
 
 /**
@@ -25,15 +24,25 @@ public class GoblinStateChase extends State {
 
     @Override
     public void update() {
-        
-        Player p = parent.gameboard.getPlayer();
 
         checkTransitions();
         
-        //Move to player
-        if (p != null){
+        //Move to target object
+        if (parent.targetObject != null){
+
+            moveTowardsSquare(parent.targetObject.getX(), parent.targetObject.getY());
             
-            calculatePath(p.getX(), p.getY());
+            //Get distance to targetObject
+            double distanceToTarget = Board.calculateDistance(parent.targetObject.getX(), parent.targetObject.getY(), parent.xposition, parent.yposition);
+
+            //If we're within melee range, attempt a melee attack
+            if (distanceToTarget <= parent.meleeWeapon.getRange()) {
+
+                parent.gameboard.getCamera().AddMessage("The goblin makes an attack    at you!");
+                
+                parent.doMeleeAttack(parent.targetObject);
+
+            }
             
         }
         
@@ -51,6 +60,26 @@ public class GoblinStateChase extends State {
         
     }
     
+    public void moveTowardsSquare(int xgoal, int ygoal) {
+        
+        Node node = Node.calculatePath(parent.xposition, parent.yposition, xgoal, ygoal, parent.gameboard);
+        
+        //Exit if path is impossible
+        if (node == null) {
+            
+            return;
+            
+        }
+
+        //Return the next node in the path
+        while (node.parent.parent != null){
+            node = node.parent;
+        }
+
+        move(node.x, node.y);
+        
+    }
+    
     public void move(int xgoal, int ygoal){
 
         //If space is not occupied by a solid object, move into it
@@ -61,83 +90,5 @@ public class GoblinStateChase extends State {
         }
         
     }
-    
-    //Pathfinding algorithm using A*
-    public void calculatePath(int xgoal, int ygoal){
-        
-        //Create node to represent target
-        Node target = new Node(xgoal, ygoal, 0);
-        
-        //Initialize list for open squares and add starting square
-        PriorityQueue<Node> openList = new PriorityQueue<>();
-        openList.add(new Node(parent.xposition, parent.yposition, 0));
-        
-        //Initialize list for closed squares
-        PriorityQueue closedList = new PriorityQueue<>();
-        
-        //While the open list is not empty
-        while (!openList.isEmpty()){
-            
-            //Get the node that is currently closest to the goal
-            Node n = openList.peek();
-            
-            //Get q's four neighbors
-            Node[] neighbors = new Node[4];
-            neighbors[0] = new Node(n.x + 1, n.y, 0);
-            neighbors[1] = new Node(n.x - 1, n.y, 0);
-            neighbors[2] = new Node(n.x, n.y + 1, 0);
-            neighbors[3] = new Node(n.x, n.y - 1, 0);
-            
-            //Go through each neighor
-            for (Node m : neighbors) {
 
-                //If the node isn't in the closed list or open list
-                if(!Node.containsNode(openList, m) && !Node.containsNode(closedList, m)){
-                    
-                    //If the node is the target, exit
-                    if ((m.x == xgoal) && (m.y == ygoal)){
-                        
-                        m.parent = n;
-                        
-                        //Return the next node in the path
-                        while (m.parent.parent != null){
-                            m = m.parent;
-                        }
-
-                        move(m.x, m.y);
-
-                        return;
-                    }
-                    
-                    //If the node is occupyied by a solid object, add it directly to the closed list
-                    if (parent.gameboard.checkIfSquareHasSolid(m.x, m.y)) {
-                    //if (((gameboard.GetObjectAtSquare(m.x, m.y)) != null) &&
-                    //!(gameboard.GetObjectAtSquare(m.x, m.y).getClass().getSimpleName().equals(this.getClass().getSimpleName()))) {
-                        
-                        closedList.add(m);
-                        
-                    }
-                    
-                    //Else, calculate its heuristic and add it to the openlist
-                    else {
-
-                        m.parent = n;
-                        m.f = m.calculateHeuristic(target);
-                        openList.add(m);
-                        
-                    }
-                    
-                }
-                
-            }
-            
-            //Move current node to closedList
-            openList.remove(n);
-            closedList.add(n);
-            
-        }
-        
-        return;
-    }
-    
 }
