7da7fb78c9112a5f9c0f652bdb61a80187ed2ed7
Anthony Howell <anthonyamh@gmail.com>
2021-07-29 19:04:37 -0500

    Testing of BSP

--- src/roguelike/Roguelike.java
+++ src/roguelike/Roguelike.java
@@ -1,25 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package roguelike;
 
-import mapgeneration.DungeonLayoutGenerator;
+import java.util.List;
+import mapgeneration.PerlinNoise;
+import mapgeneration.bsp.BSP;
+import mapgeneration.bsp.BSPnode;
 import view.Camera;
 
 /**
- *
  * @author Anthony
  */
 public class Roguelike {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
         Camera c = new Camera(null, null, 35);
-        DungeonLayoutGenerator dg = new DungeonLayoutGenerator();
+        Board gameboard = new Board(500);
+        int[][] scene = new int[51][51];
+
+        BSP binarySpacePartition = new BSP(0, 0, 50, 50, 3);
+
+        List<BSPnode> nodes = binarySpacePartition.getEndNodes(4);
+        
+        for (BSPnode node : nodes) {
+            
+            System.out.print("(" + node.getX() + ", " + node.getY() + ") x ");
+            System.out.print("(" + (node.getX() + node.getW()) + ", " + node.getY() + ") x ");
+            System.out.print("(" + (node.getX() + node.getW()) + ", " + (node.getY() + node.getH()) + ") x ");
+            System.out.println("(" + node.getX() + ", " + (node.getY() + node.getH()) + ")");
+            
+            for (int i = node.getX(); i < node.getX() + node.getW(); i++) {
+                
+                scene[i][node.getY()] = 1;
+                
+            }
+            
+            for (int i = node.getY(); i < node.getY() + node.getH(); i++) {
+                
+                scene[node.getX()][i] = 1;
+                
+            }
+            
+            for (int i = node.getX(); i < node.getX() + node.getW(); i++) {
+                
+                scene[i][node.getY() + node.getH()] = 1;
+                
+            }
+            
+            for (int i = node.getY(); i < node.getY() + node.getH(); i++) {
+                
+                scene[node.getX() + node.getW()][i] = 1;
+                
+            }
+            
+        }
+        
+        for (int x = 0; x < scene.length; x++) {
+            
+            for (int y = 0; y < scene.length; y++) {
+                
+                if (scene[x][y] == 0) {
+                    
+                    System.out.print("  ");
+                    
+                    continue;
+                    
+                }
+                
+                System.out.print("# ");
+            
+            }
+            
+            System.out.println();
+            
+        }
+        
     }
     
 }
